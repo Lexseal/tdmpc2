f8c0a5e3dd017bb8ba09c2afcc9bce1b816987b3
t-sekai <tsekaichan@gmail.com>
2024-05-20 02:06:53 -0700

    fix some bugs

--- tdmpc2/trainer/online_trainer.py
+++ tdmpc2/trainer/online_trainer.py
@@ -142,7 +142,7 @@ class OnlineTrainer(Trainer):
 				obs = self.env.reset()
 				self._tds = [self.to_td(obs)]
 
-			horizon = self.get_horizon(self._step, consistency_loss_history, reward_loss_history, value_loss_history)
+			horizon = self.get_horizon(self._step, consistency_loss_history, reward_loss_history, value_loss_history, total_loss_history)
 			# Collect experience
 			if self._step > self.cfg.seed_steps:
 				action = self.agent.act(obs, t0=len(self._tds)==1, horizon=horizon)
