2e44f4c48234ac81d1d81b4c89e32beb4fa1cd49
Xinsong Lin <zhongjiezhex12@me.com>
2024-05-19 21:39:26 -0700

    bug fix

--- tdmpc2/trainer/online_trainer.py
+++ tdmpc2/trainer/online_trainer.py
@@ -1,4 +1,5 @@
 from time import time
+from collections import deque
 
 import numpy as np
 import torch
@@ -15,6 +16,7 @@ class OnlineTrainer(Trainer):
 		self._step = 0
 		self._ep_idx = 0
 		self._start_time = time()
+		self.last_horizon = self.cfg.min_horizon
 
 	def common_metrics(self):
 		"""Return a dictionary of current metrics."""
@@ -64,21 +66,33 @@ class OnlineTrainer(Trainer):
 		), batch_size=(1,))
 		return td
 
-	def get_horizon(self, steps, increase_stop=150_000, decrease_stop=200_000):
+	def get_horizon(self, steps, consistency_loss_history, increase_stop=150_000):
 		"""
 		Return the horizon for the current step, starting at self.cfg.min_horizon and ending at self.cfg.max_horizon
 		We will just do a linear interpolation between the two values.
 		"""
-		if steps >= decrease_stop:
-			return self.cfg.horizon
-		elif steps >= increase_stop:
-			# do a linear interpolation between max_horizon and horizon
-			return int(self.cfg.max_horizon + (self.cfg.horizon - self.cfg.max_horizon) * (steps - increase_stop) / (decrease_stop - increase_stop))
-		return int(self.cfg.min_horizon + (self.cfg.max_horizon - self.cfg.min_horizon) * steps / increase_stop)
+		new_horizon = int(self.cfg.min_horizon + (self.cfg.max_horizon - self.cfg.min_horizon) * steps / increase_stop)
+		if steps >= increase_stop:
+			if steps % 5000 == 0:
+				# assume consistency_loss_history is full and has 10000 elements
+				first_half_mean = np.mean(list(consistency_loss_history)[:5000])
+				second_half_mean = np.mean(list(consistency_loss_history)[5000:])
+				if second_half_mean < first_half_mean:
+					new_horizon = self.last_horizon - 2
+				else:
+					new_horizon = self.last_horizon + 1
+			else:
+				new_horizon = self.last_horizon
+		new_horizon = max(self.cfg.min_horizon, min(self.cfg.max_horizon, new_horizon))
+		self.last_horizon = new_horizon
+		return new_horizon
 
 	def train(self):
 		"""Train a TD-MPC2 agent."""
 		train_metrics, done, eval_next = {}, True, True
+		# keep track of 5000 steps moving average of consistency loss
+		# to do that need 10000 steps of buffer. use deque
+		consistency_loss_history = deque(maxlen=10000)
 		while self._step <= self.cfg.steps:
 			# Evaluate agent periodically
 			if self._step % self.cfg.eval_freq == 0:
@@ -104,7 +118,7 @@ class OnlineTrainer(Trainer):
 				obs = self.env.reset()
 				self._tds = [self.to_td(obs)]
 
-			horizon = self.get_horizon(self._step)
+			horizon = self.get_horizon(self._step, consistency_loss_history)
 			# Collect experience
 			if self._step > self.cfg.seed_steps:
 				action = self.agent.act(obs, t0=len(self._tds)==1, horizon=horizon)
@@ -123,6 +137,7 @@ class OnlineTrainer(Trainer):
 				for _ in range(num_updates):
 					_train_metrics = self.agent.update(self.buffer, horizon)
 				train_metrics.update(_train_metrics)
+				consistency_loss_history.append(_train_metrics['consistency_loss'])
 
 			self._step += 1
 	
